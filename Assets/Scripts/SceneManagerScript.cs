48bd54c4924c642b85ff7563d7b4b813b80cee27
cheliotk <cheliotis.k@gmail.com>
2020-02-01 18:54:09 +0200

    added UI extentions, cutscene logic

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Utility;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 
 public class SceneManagerScript : MonoBehaviour
 {
@@ -14,14 +16,45 @@ public class SceneManagerScript : MonoBehaviour
 
     PuzzleManagerScript pzMan;
 
-    void Start(){
-        isPlayingIntro = true;
+    Canvas canvas;
+    public GameObject cutsceneOverlay;
+    public UICircle progressCircle;
+    public Text cutsceneText;
+    public GameObject crosshairUI;
+    public bool hasCrosshair = true;
+    float cutsceneStartTime = -1f;
 
+    void Start(){
+        Cursor.visible = false;
+        isPlayingIntro = true;
+        cutsceneStartTime = Time.timeSinceLevelLoad;
+        cutsceneText.text = "INTRO CUTSCENE IN PROGRESS";
+        
         Invoke("SetupPlayMode", introDuration);
     }
 
+    void Update(){
+        if (isPlayingIntro){
+            // cutsceneOverlay.SetActive(true);
+            float cutsceneProgress = (Time.timeSinceLevelLoad - cutsceneStartTime) / introDuration;
+            progressCircle.SetProgress(cutsceneProgress);
+        }
+        else if(isPlayingOutro){
+            // cutsceneOverlay.SetActive(true);
+            float cutsceneProgress = (Time.timeSinceLevelLoad - cutsceneStartTime) / introDuration;
+            progressCircle.SetProgress(cutsceneProgress);
+            if(cutsceneProgress > 1f){
+                //LOAD NEXT LEVEL
+            }
+        }
+    }
+
     void SetupPlayMode(){
+        if(hasCrosshair){
+            crosshairUI.SetActive(true);
+        }
         isPlayingIntro = false;
+        cutsceneOverlay.SetActive(false);
         GameObject.FindObjectOfType<PlayerInputScript>().Init();
         GameObject.FindObjectOfType<CameraControlScript>().Init();
         GameObject.FindObjectOfType<ItemInteractionScript>().Init();
@@ -34,12 +67,18 @@ public class SceneManagerScript : MonoBehaviour
     }
 
     public void PuzzleSolved(){
-        isPlayingOutro = true;
+        
 
         Invoke("PlayOutro", 1f);
     }
 
     void PlayOutro(){
+        cutsceneOverlay.SetActive(true);
+        isPlayingOutro = true;
+        cutsceneStartTime = Time.timeSinceLevelLoad;
+        cutsceneText.text = "OUTRO CUTSCENE IN PROGRESS";
         GameObject.FindObjectOfType<SimpleMouseRotator>().End();
+        GameObject.FindObjectOfType<PlayerInputScript>().End();
+        GameObject.FindObjectOfType<ItemInteractionScript>().End();
     }
 }
